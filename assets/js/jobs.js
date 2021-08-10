70e259ec3f17bc45e0a884a9ba1a0ad3c55b2921
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-08-10 21:54:35 +0000

    Update jobs.js

--- assets/js/jobs.js
+++ assets/js/jobs.js
@@ -5,14 +5,30 @@ $(document).ready(function(){
 $('.viewItem').click(function(){
     let id = this.id;
     //console.log(id);
-    getDataByID(id)
+    getDataByID(id);
+
 })
 
 
 function getDataByID(id){
     let bigData = data;
     let res = bigData.filter((record, index) => record.Id == id);
-    console.log(res)
+    //console.log(res[0].Client)
+    $('#clientNameU').val(res[0].Client);
+    $('#phoneU').val(res[0].Phone);
+    $('#locationU').val(res[0].Location);
+    $('#modelU').val(res[0].Model);
+    $('#prodYearU').val(res[0].YearOfMake);
+    $('#problemU').val(res[0].Problem);
+    $('#estimatedCostU').val(res[0].EstimatedCost);
+    $('#paymentAmountU').val(res[0].AmountPaid);
+    $('#numberPlateU').val(res[0].PlateNo);
+    $('#estimatedDateOfCompletionU').val(res[0].EstimatedDate);
+    $('#jobStatusU option:selected').val(res[0].JobStatus); $('#jobStatusU option:selected').text(res[0].JobStatus);
+    $('#transmissionU option:selected').val(res[0].Transmission); $('#transmissionU option:selected').text(res[0].Transmission); 
+    $('#carTypeU option:selected').val(res[0].Type); $('#carTypeU option:selected').text(res[0].Type);
+    $('#bodyColorU option:selected').val(res[0].Color);     $('#bodyColorU option:selected').text(res[0].Color);    
+
 }
    
    
